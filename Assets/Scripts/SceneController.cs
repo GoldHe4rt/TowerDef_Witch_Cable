26ae4f02ad0eb8ae8dc486574380517b48abfec5
Fenris <fenris.dyrendal@gmail.com>
2026-04-17 11:08:56 +0200

    Started SceneController work
    Have started code work for the buttons. Will now look into programming for settings

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    //Used for START_BUTTON (might change name). Loads the player selection. No scene yet.
+    //public void SelectPlayerScene() { SceneManager.LoadScene("Player_Select"); }
+
+    //To be used after players have selected their characters. Loads level1.
+    //Temporarily used to start game before we have player selection in place.
+    public void StartGame()
+    {
+        Debug.Log("StartButton was pressed");
+        SceneManager.LoadScene("Level 1");
+    }
+    
+    //Used for MAIN_MENU_BUTTON. Loads Main_menu Scene.
+    public void MainMenu()
+    {
+        Debug.Log("MainMenuButton was pressed");
+        SceneManager.LoadScene("Main_menu");
+    }
+    
+    //Used for SETTINGS_BUTTON. Loads the settings scene. No scene yet.
+    //public void SettingsScene() { SceneManager.LoadScene("Settings"); }
+
+    //Run this when EXIT_BUTTON or ESC is pressed
+    public void Exit()
+    {
+        Debug.Log("ExitButton was pressed");
+        Application.Quit();
+        if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
+    }
+}
