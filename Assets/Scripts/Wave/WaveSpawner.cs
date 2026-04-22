e68416aa9ce02414c7a211836cd52bfd24d68eef
GoldHe4rt <marius.eftang@gmail.com>
2026-04-22 10:24:56 +0200

    Wave script
    Wave script update

--- Assets/Scripts/Wave/WaveSpawner.cs
+++ Assets/Scripts/Wave/WaveSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 [System.Serializable]
 public class Wave
@@ -11,4 +12,19 @@ public class Wave
 public class WaveSpawner : MonoBehaviour
 {
     [SerializeField] Wave[] waves;
+    [SerializeField] Transform[] spawnPoints;
+
+    [SerializeField] Wave currentWave;
+    [SerializeField] int currentWaveNumber;
+
+    private void Update()
+    {
+        currentWave = waves[currentWaveNumber]
+    }
+
+    void SpawnWave()
+    {
+        GameObject randomEnemy = currentWave.typeOfEnemies[Random.range(0, currentWave.typeOfEnemies.Length)];
+        Transform randomPoint =
+    }
 }
