7c985f52f73d96d6d456ab083687db3d4e73367e
Fenris <fenris.dyrendal@gmail.com>
2026-05-13 19:57:20 +0200

    Prefab issue now actually fixed
    Settings button for pop-up no longer works :( will fix somehow

--- Assets/Scripts/Menu/SettingsPopupManager.cs
+++ Assets/Scripts/Menu/SettingsPopupManager.cs
@@ -5,20 +5,27 @@ namespace Menu
     public class SettingsPopupManager : MonoBehaviour
     {
         public GameObject settingsMenu;
-        [SerializeField] private PauseManager pauseManager;
+        private PauseManager pauseManager;
+        public bool settingsActive;
 
         private void Start()
-        { settingsMenu.SetActive(false); }
+        {
+            settingsMenu.SetActive(false);
+            settingsActive = false;
+        }
 
         public void ShowSettings()
         {
-            pauseManager.ForcePauseWithoutMenu();
+            Debug.Log("Settings button was pressed");
             settingsMenu.SetActive(true);
+            settingsActive = false;
+            pauseManager.ForcePauseWithoutMenu();
         }
 
         public void HideSettings()
         {
             settingsMenu.SetActive(false);
+            settingsActive = false;
             pauseManager.canPause = true;
             pauseManager.Pause();
         }
