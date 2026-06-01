1aae840776881d88be042de402441fdd33371e01
Fenris <fenris.dyrendal@gmail.com>
2026-06-01 14:30:23 +0200

    DifficultySettings ScriptableObject script started
    can be used to make scriptableobjects for easy, normal etc once the changing values and such have been put in the script

--- Assets/Scripts/Difficulty/SelectDifficulty.cs
+++ Assets/Scripts/Difficulty/SelectDifficulty.cs
@@ -1,6 +1,46 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SelectDifficulty : MonoBehaviour
 {
-    
+    private readonly string difficultyKey;
+
+    private void Start()
+    {
+        PlayerPrefs.GetFloat(difficultyKey);
+    }
+
+    //For easy button
+    public void SetToEasy()
+    {
+        
+        OnDifficultyChanged();
+    }
+
+    //For normal button
+    public void SetToNormal()
+    {
+        OnDifficultyChanged();
+    }
+
+    //For hard button
+    public void SetToHard()
+    {
+        
+        OnDifficultyChanged();
+    }
+
+    //For nightmare button
+    public void SetToNightmare()
+    {
+        
+        OnDifficultyChanged();
+    }
+
+    private void OnDifficultyChanged()
+    {
+        
+        PlayerPrefs.Save();
+    }
 }
