e7d421b7ef60362b43423d3fad376840cc4f475f
Nathanael17671 <nathanael17671@gmail.com>
2026-06-02 10:32:58 +0200

    Gizmo Arrows

--- Assets/Scripts/Enemies/Movement/Waypoint.cs
+++ Assets/Scripts/Enemies/Movement/Waypoint.cs
@@ -24,7 +24,20 @@ public class Waypoint : MonoBehaviour
         {
             if (waypoint != null)
             {
+                Gizmos.color = Color.red;
                 Gizmos.DrawLine(transform.position, waypoint.transform.position);
+                //Direction arrow Triangle
+                Gizmos.color = Color.blue;
+                Vector3 direction = (waypoint.transform.position - transform.position).normalized;
+                Vector3 perpendicular = Vector3.Cross(direction, Vector3.forward).normalized;
+                float arrowSize = 2f;
+                float arrowDistance = 0.5f;
+                Vector3 arrowTip = transform.position + direction * arrowSize;
+                Vector3 arrowLeft = transform.position + direction * (arrowSize * 0.5f) + perpendicular * (arrowSize * 0.25f);
+                Vector3 arrowRight = transform.position + direction * (arrowSize * 0.5f) - perpendicular * (arrowSize * 0.25f);
+                Gizmos.DrawLine(transform.position + direction * arrowDistance, arrowTip);
+                Gizmos.DrawLine(arrowTip, arrowLeft);
+                Gizmos.DrawLine(arrowTip, arrowRight);
             }
         }
         if (waypoints.Length == 0)
