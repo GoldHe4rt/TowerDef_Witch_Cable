db15038a489cba99c1743686bccd1a76ba7874d0
Fenris <fenris.dyrendal@gmail.com>
2026-06-04 13:47:13 +0200

    Added in UI for health + HealthUI script to make hearts work
    For use in the Player prefab

---
+++ Assets/Scripts/UI/HealthUI.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthUI : MonoBehaviour
+{
+    public GameObject[] lifeUI;
+    public UnityEvent loseLifeEvent;
+    private PlayerHealth playerHealth;
+    private int currentHealth;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        loseLifeEvent ??= new UnityEvent();
+
+        foreach (var life in lifeUI) life.SetActive(true);
+
+        loseLifeEvent.AddListener(LoseLife);
+        
+    }
+
+    private void UpdateHealth(int healthAmount)
+    {
+        currentHealth = healthAmount;
+        loseLifeEvent.Invoke();
+    }
+    private void LoseLife()
+    {
+        switch (currentHealth)
+        {
+            case 5:
+            {
+                foreach (var life in lifeUI) life.SetActive(true);
+                break;
+            }
+            case 4:
+                lifeUI[4].SetActive(false);
+                break;
+            case 3:
+                lifeUI[4].SetActive(false);
+                lifeUI[3].SetActive(false);
+                break;
+            case 2:
+                lifeUI[4].SetActive(false);
+                lifeUI[3].SetActive(false);
+                lifeUI[2].SetActive(false);
+                break;
+            case 1:
+                lifeUI[4].SetActive(false);
+                lifeUI[3].SetActive(false);
+                lifeUI[2].SetActive(false);
+                lifeUI[1].SetActive(false);
+                break;
+            case 0:
+                lifeUI[4].SetActive(false);
+                lifeUI[3].SetActive(false);
+                lifeUI[2].SetActive(false);
+                lifeUI[1].SetActive(false);
+                lifeUI[0].SetActive(false);
+                break;
+        }
+    }
+}
