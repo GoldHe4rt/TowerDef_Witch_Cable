6931af4de8315382f2137ba0a1cc4e4b67fc5974
Fenris <fenris.dyrendal@gmail.com>
2026-06-05 10:55:57 +0200

    LevelSelect and PlaySelect set up

--- Assets/Scripts/Difficulty/DifficultySettings.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DifficultySettings", menuName = "Scriptable Objects/DifficultySettings")]
-public class DifficultySettings : ScriptableObject
-{
-    
-}
