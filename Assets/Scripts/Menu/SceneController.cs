0d7376b1b50787df3d277f446a0fc3bba3d9157f
Fenris <fenris.dyrendal@gmail.com>
2026-06-09 21:44:28 +0200

    Small UI changes, Info scene in Build
    +new code stuff in SceneController that needs testing. If it works then we'll have a good next level button (works with names until we have a cleaner build set up)

--- Assets/Scripts/Menu/SceneController.cs
+++ Assets/Scripts/Menu/SceneController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,7 +9,11 @@ namespace Menu
         [SerializeField] private bool requirePlayers;
         [SerializeField] private int requiredPlayerCount = 1;
         [SerializeField] private ControllerManager _controllerManager;
-        
+        [SerializeField] private String currentSceneName;
+
+        private void Start()
+        { currentSceneName = SceneManager.GetActiveScene().name; }
+
 
         private bool EnoughPlayers()
         { 
@@ -21,11 +26,12 @@ namespace Menu
         public void ChooseLevel()
         { if (EnoughPlayers()) SceneManager.LoadScene("LevelSelect"); else Debug.Log("Not enough players"); }
 
+        public void InfoScene()
+        { SceneManager.LoadScene("Info"); }
         public void MariusLevel()
         { if (EnoughPlayers()) SceneManager.LoadScene("Marius"); else Debug.Log("Not enough players"); }
 
-        public void Tutorial()
-        { if(EnoughPlayers()) SceneManager.LoadScene("Tutorial"); else Debug.Log("Not enough players"); }
+        //public void Tutorial() { if(EnoughPlayers()) SceneManager.LoadScene("Tutorial"); else Debug.Log("Not enough players"); }
 
         public void Level1()
         { if(EnoughPlayers()) SceneManager.LoadScene("Level 1"); else Debug.Log("Not enough players"); }
@@ -39,9 +45,22 @@ namespace Menu
         public void Restart()
         { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); }
 
-        //Used for NEXT_LEVEL_BUTTON in the Win Menu. Will need changes to make this work.
+        //Used for NEXT_LEVEL_BUTTON in the Win Menu. Need to test to see if this works.
         public void NextLevel()
-        { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); }
+        {
+            switch (currentSceneName)
+            {
+                case "Level 1":
+                    SceneManager.LoadScene("Level 2");
+                    break;
+                case "Level 2":
+                    SceneManager.LoadScene("Level 3");
+                    break;
+                case "Level 3":
+                    SceneManager.LoadScene("LevelSelect");
+                    break;
+            }
+        }
     
         public void MainMenu()
         { SceneManager.LoadScene(sceneBuildIndex: 0); }
