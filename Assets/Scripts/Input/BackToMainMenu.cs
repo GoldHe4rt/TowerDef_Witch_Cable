e88e054281522cf57131d25f23bb0d429e090a6b
Fenris <fenris.dyrendal@gmail.com>
2026-06-11 18:00:50 +0200

    Pickups and stuff

--- Assets/Scripts/Input/BackToMainMenu.cs
+++ Assets/Scripts/Input/BackToMainMenu.cs
@@ -1,5 +1,3 @@
-using System;
-using Menu;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -7,14 +5,12 @@ using UnityEngine.SceneManagement;
 public class BackToMainMenu : MonoBehaviour
 {
     [SerializeField] private InputActionReference backAction;
-
-
+    
     private void Update()
     {
-        if (backAction.ToInputAction().triggered)
-        {
-            SceneManager.LoadScene(sceneBuildIndex: 0);
-        }
+        if (backAction.ToInputAction().triggered && SceneManager.GetActiveScene().name == "LevelSelect")
+            SceneManager.LoadScene("PlayerSelect");
+        else if (backAction.ToInputAction().triggered) SceneManager.LoadScene(sceneBuildIndex: 0);
     }
     
 }
