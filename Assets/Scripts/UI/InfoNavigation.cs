543682eaa556e753b8e30eeefbdb0a45e890a12c
Fenris <fenris.dyrendal@gmail.com>
2026-06-12 13:47:45 +0200

    Start functionality for tutorial/info stuff

---
+++ Assets/Scripts/UI/InfoNavigation.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoNavigation : MonoBehaviour
+{
+    [SerializeField] private Button nextButton;
+    [SerializeField] private List<GameObject> infoUI;
+    private string levelselectKey;
+
+
+
+    
+
+}
